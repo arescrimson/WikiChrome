653ba9ce205db5e2604939e1742364f215550698
areszhang25 <areszee7@gmail.com>
2023-11-01 15:14:11 -0700

    Update contentScript.ts
    Updated function and exports for testing functionality

--- src/scripts/contentScript.ts
+++ src/scripts/contentScript.ts
@@ -1,27 +1,35 @@
 // contentScript.ts
 
-getData()
+export {}
+
+getSelection()
 
 /**
  * Gets Data from currently highlighted text in window. Sends wikipedia json to background. 
  */
-async function getData() {
-    const selection = window.getSelection();
 
-    if (selection) {
-        const selectionText: string = selection.toString();
+function getSelection() {
+    const selection: any = window.getSelection()
 
-        if (selectionText) {
-            const searchURL: string = `https://en.wikipedia.org/api/rest_v1/page/summary/${selectionText}`;
+    if (selection) getData(selection.toString())
+}
 
-            try {
-                const response = await fetch(searchURL);
-                const data = await response.json()
+async function getData(selectionText: string) {
 
-                chrome.runtime.sendMessage({ wikipediaData: data });
-            } catch (error) {
-                console.log("Error fetching data from Wikipedia API in contentScript:" + error);
-            };
-        }
-    }
+    if (!selectionText) return;
+
+    const searchURL: string = `https://en.wikipedia.org/api/rest_v1/page/summary/${selectionText}`;
+
+    try {
+        const response = await fetch(searchURL);
+        const data = await response.json()
+
+        // chrome.runtime.sendMessage({ wikipediaData: data });
+    } catch (error) {
+        console.log("Error fetching data from Wikipedia API in contentScript:" + error);
+    };
+}
+
+module.exports = {
+    getData
 }
