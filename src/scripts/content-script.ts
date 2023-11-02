5716a477785290b715c6bcae5c192248e98bfb1e
areszhang25 <areszee7@gmail.com>
2023-11-01 22:44:10 -0700

    Create content-script.ts
    Renamed

---
+++ src/scripts/content-script.ts
@@ -0,0 +1,38 @@
+// contentScript.ts
+
+export {}
+
+getSelection()
+
+/**
+ * Gets Data from currently highlighted text in window. Sends wikipedia json to background. 
+ */
+
+function getSelection() {
+    const selection: any = window.getSelection()
+
+    if (selection) {
+        getData(selection.toString())
+        return
+    }
+}
+
+async function getData(selectionText: string) {
+
+    if (!selectionText) return;
+
+    const searchURL: string = `https://en.wikipedia.org/api/rest_v1/page/summary/${selectionText}`;
+
+    try {
+        const response = await fetch(searchURL);
+        const data = await response.json()
+
+        chrome.runtime.sendMessage({ wikipediaData: data });
+    } catch (error) {
+        console.log("Error fetching data from Wikipedia API in contentScript:" + error);
+    };
+}
+
+module.exports = {
+    getData
+}
