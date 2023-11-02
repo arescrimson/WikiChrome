94ad661242b7ea22330c55712d3745370b05e712
areszhang25 <areszee7@gmail.com>
2023-11-01 22:44:34 -0700

    Update webpack.config.js
    Update webpack html and css behavior

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
    mode: "production",
    entry: {
       background: path.resolve(__dirname, "..", "src", "background.ts"),
       popup: path.resolve(__dirname, "..", "src", "popup", "popup.ts"),
-      contentScript: path.resolve(__dirname, "..", "src", "scripts", "contentScript.ts"),
+      contentScript: path.resolve(__dirname, "..", "src", "scripts", "content-script.ts"),
    },
    output: {
       path: path.join(__dirname, "../dist"),
@@ -21,12 +24,27 @@ module.exports = {
             loader: "ts-loader",
             exclude: /node_modules/,
          },
-      ],
+         {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+            ],
+        },
+      ], 
+      
    },
    plugins: [
       new CopyPlugin({
          patterns: [{from: ".", to: ".", context: "public"}]
       }),
+      new HtmlWebpackPlugin({
+         template: './src/popup/popup.html', // HTML template in src
+         filename: 'popup.html', // Output HTML filename
+     }),
+     new MiniCssExtractPlugin({
+         filename: 'style.css',
+     }),
    ],
    watch: true, 
 };
